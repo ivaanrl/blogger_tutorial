8b5c18a932c8a5373edcb89f7ae481727f8350eb
ivaanrl <ivanrl2010@gmail.com>
2019-09-19 08:50:04 -0300

    first blogger commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,21 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   def index 
     @articles = Article.all 
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to article_path(@article)
   end
 end
